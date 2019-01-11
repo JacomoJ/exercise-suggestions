4c044796953ffcca453b1c537158a924190be112
Joey <bevilacquajoey@gmail.com>
2019-01-11 10:55:28 +0100

    not so easy: add solution for the audio player
    Signed-off-by: Joey <bevilacquajoey@gmail.com>

--- write-code/sol_not-so-easy.rkt
+++ write-code/sol_not-so-easy.rkt
@@ -44,13 +44,40 @@
 
 (define TEST-LIST (list (make-song "Deep Impact" "DJ Krush" "Reload" 267)
                         (make-song "We the people" "A tribe called quest" "We got it from here" 172)
-                        (make-song "You can't go home again" "DJ Shadow" "Reconstructed" 423)))
+                        (make-song "You can't go home again" "DJ Shadow" "Reconstructed" 423)
+                        (make-song "Never gonna give you up" "Rick Astley" "Whenever you need somebody" 199)
+                        (make-song "Cry for help" "Rick Astley" "Free" 241)
+                        (make-song "Together forever" "Rick Astley" "Whenever you need somebody" 195)))
 
-(check-expect (no-songs TEST-LIST) 3)
+(check-expect (no-songs TEST-LIST) 6)
 
 ; List<Song> -> Number
 ; Purpose: Add the length of all songs in the list
 (define (total-length ls) (cond [(equal? ls '()) 0]
                                 [else (+ (song-length (first ls)) (total-length (rest ls)))]))
 
-(check-expect (total-length TEST-LIST) (+ (+ 267 172) 423))
+(check-expect (total-length TEST-LIST) (+ (+ (+ (+ (+ 267 172) 423) 199) 241) 195))
+
+; T is a Type
+; List<T> lambda(T -> Boolean) -> List<T>
+; Purpose: Given a list and a boolean lambda, return a list which contains all
+; the elements of the first list that satisfy the lambda function
+(define (filter-out to-be-filtered list-filter)
+  (cond [(empty? to-be-filtered) '()]
+        [else (cond [(list-filter (first to-be-filtered))
+                     (cons (first to-be-filtered) (filter-out (rest to-be-filtered) list-filter))]
+                    [else (filter-out (rest to-be-filtered) list-filter)])]))
+
+(check-expect (filter-out (list 1 2 3 4 5 6 7) (lambda (x) (= 0 (modulo x 2))))
+              (list 2 4 6))
+
+; List<Song>  Artist Album-> List<Song>
+; Purpose: Filter out of a given list of songs basing on album and artist
+(define (filter-by-album-artist ls album artist)
+    (filter-out ls (lambda (x) (and (string=? album (song-album x))
+                                    (string=? artist (song-artist x))))))
+
+(define TEST-FILTER (list (make-song "Never gonna give you up" "Rick Astley" "Whenever you need somebody" 199)
+                          (make-song "Together forever" "Rick Astley" "Whenever you need somebody" 195)))
+
+(check-expect (filter-by-album-artist TEST-LIST "Whenever you need somebody" "Rick Astley") TEST-FILTER)
